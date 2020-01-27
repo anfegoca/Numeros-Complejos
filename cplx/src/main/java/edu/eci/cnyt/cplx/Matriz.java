c4021f1754a9e7729357ec3b7aabf7bebb88c92d
unknown <andres.gonzalez-ca@labinfo.is.escuelaing.edu.co>
2020-01-27 15:55:23 -0500

    Matriz conjugada

--- cplx/src/main/java/edu/eci/cnyt/cplx/Matriz.java
+++ cplx/src/main/java/edu/eci/cnyt/cplx/Matriz.java
@@ -19,14 +19,81 @@ public class Matriz {
         n=numeros.length;
         m=numeros[0].length;
     }
+    public boolean equals(Object obj){
+        Matriz mat = (Matriz)obj;
+        boolean res = true;
+        if(mat.getN()!=n || mat.getM()!=m){
+            res=false;
+        }else{
+            Complex[][] aux= mat.getNumeros();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(!(aux[i][j].equals(numeros[i][j]))){
+                        res=false;
+                        break;
+                    }
+                }
+        }
+        }
+        return res;
+    }
+    
     
     public Matriz suma(Matriz mat){
+        Matriz res=null;
         if(mat.getN()!=n || mat.getM()!=m){
             new ExcepcionDim(ExcepcionDim.mensajeDimMat);
         }else{
+            Complex[][] aux2 = new Complex[n][m];
             Complex[][] aux= mat.getNumeros();
-            
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    aux2[i][j]=aux[i][j].suma(numeros[i][j]);
+                }
+            }
+            res = new Matriz(aux2);
         }
+        return res;
+    }
+    public Matriz inversa(){
+        Complex[][] aux = new Complex[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                aux[i][j]=numeros[i][j].inverso();    
+            }
+        }
+        Matriz res = new Matriz(aux);
+        return res;
+    }
+    public Matriz productoEsca(Complex esc){
+        Complex[][] aux = new Complex[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                aux[i][j]=numeros[i][j].producto(esc);
+            }
+        }
+        Matriz res = new Matriz(aux);
+        return res;
+    }
+    public Matriz transpuesta(){
+        Complex[][] aux = new Complex[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                aux[i][j]=numeros[j][i];    
+            }
+        }
+        Matriz res = new Matriz(aux);
+        return res;
+    }
+    public Matriz conjugada(){
+        Complex[][] aux = new Complex[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                aux[i][j]=numeros[i][j].conjugado();    
+            }
+        }
+        Matriz res = new Matriz(aux);
+        return res;
     }
     public int getN(){
         return n;  
